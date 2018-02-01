d910aa22cefb5324b8cca528000fa8c79a1f9175
Daniel Sellers <dsellers@instructure.com>
2018-02-01 06:03:07 -0700

    added -0 cause who knew that was a thing

--- fib.js
+++ fib.js
@@ -3,6 +3,10 @@
         throw new Error('TypeError');
     }
 
+    if (i === -0) {
+        return 0;
+    }
+
     return i <= 1 ? i : getFib(i - 1) + getFib(i - 2);
 }
 
--- fib.test.js
+++ fib.test.js
@@ -4,6 +4,7 @@ const assert = require('chai').assert;
 describe('fib tests', () => {
     it('should return 0 for 0', () => {
         assert.strictEqual(getFib(0), 0);
+        assert.strictEqual(getFib(-0), 0);
     });
 
     it('should return 1 for 1', () => {
