c2ca0da918235d6df5e7e09c18f870b41f4b3bc6
Daniel Sellers <dsellers@instructure.com>
2018-02-01 18:30:29 -0700

    removed most of ths tests for learning;

--- fib.test.js
+++ fib.test.js
@@ -6,15 +6,5 @@ describe('fib tests', () => {
         assert.strictEqual(getFib(0), 0);
     });
 
-    it('should return 1 for 1', () => {
-        assert.strictEqual(getFib(1), 1);
-    });
-
-    it('should return 89 for 11', () => {
-        assert.strictEqual(getFib(11), 89);
-    });
-
-    it('should return 357 for 14', () => {
-        assert.strictEqual(getFib(14), 377);
-    });
+    // let's write some tests here!
 });
