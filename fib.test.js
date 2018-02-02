2171bff5fc2a2b8b491c3d53630003981044b4ab
Daniel Sellers <dsellers@instructure.com>
2018-02-01 18:07:37 -0700

    collapse the throw errors into a loop over an array

--- fib.test.js
+++ fib.test.js
@@ -19,28 +19,26 @@ describe('fib tests', () => {
         assert.strictEqual(getFib(14), 377);
     });
 
-    it('should throw type error for negative numbers', () => {
-        assert.throws(() => getFib(-1), 'TypeError');
-        assert.throws(() => getFib(-42), 'TypeError');
-        assert.throws(() => getFib(-102), 'TypeError');
-        assert.throws(() => getFib(-Infinity), 'TypeError');
-    });
-
-    it('should throw type error for boolean', () => {
-        assert.throws(() => getFib(true), 'TypeError');
-    });
-
     it('should work for string that is a number', () => {
         assert.strictEqual(getFib('14'), 377);
         assert.strictEqual(getFib('0x05'), 5);
         assert.strictEqual(getFib('0x06'), 8);
     });
 
-    it('should throw type error for string', () => {
-        assert.throws(() => getFib('hello world'), 'TypeError');
-    });
-
-    it('should throw a type error if a function', () => {
-        assert.throws(() => getFib(() => {return `can't take the sky from me`}), 'TypeError');
+    context('should throw errors for non-number inputs', () => {
+        [
+            -1,
+            -42,
+            -102,
+            -Infinity,
+            true,
+            'the enemies gate is down',
+            () => `can't take the sky from me`,
+            false
+        ].forEach(input => {
+            it(`should throw TypeError for ${input.toString()}`, () => {
+                assert.throws(() => getFib(input), 'TypeError');
+            });
+        });
     });
 });
