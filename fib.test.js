7d9b6c5704dda5853499579859ada4be2f05bf55
Daniel Sellers <daniel.sellers@osmosis.org>
2020-08-04 21:42:29 -0600

    updated to new version

--- fib.test.js
+++ fib.test.js
@@ -5,5 +5,15 @@ describe('fib tests', () => {
         expect(getFib(0)).toBe(0);
     });
 
-    // let's write some tests here!
+    test('should return 1 for 1', () => {
+        expect(getFib(1)).toBe(1);
+    });
+
+    test('should return 89 for 11', () => {
+        expect(getFib(11)).toBe(89);
+    });
+
+    test('should return 357 for 14', () => {
+        expect(getFib(14)).toBe(377);
+    });
 });
