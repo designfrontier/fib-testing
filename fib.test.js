4b5aeeccb6a84582008f26bf572f4f41b289665b
Daniel Sellers <daniel.sellers@osmosis.org>
2020-08-04 21:38:44 -0600

    converted to jest

--- fib.test.js
+++ fib.test.js
@@ -1,9 +1,8 @@
 const getFib = require('./fib');
-const assert = require('chai').assert;
 
 describe('fib tests', () => {
-    it('should return 0 for 0', () => {
-        assert.strictEqual(getFib(0), 0);
+    test('should return 0 for 0', () => {
+        expect(getFib(0)).toBe(0);
     });
 
     // let's write some tests here!
